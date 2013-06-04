d917d23c5525c89e445e21a52ac3abb650427261
Andrew Fecheyr <andrew@bedesign.be>
2013-06-04 16:32:04 +0200

    Inital clean berks cookbook commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# crypto-coin cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Andrew Fecheyr (<andrew@bedesign.be>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "crypto-coin"
+maintainer       "Andrew Fecheyr"
+maintainer_email "andrew@bedesign.be"
+license          "MIT"
+description      "Installs/Configures crypto-coin"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
