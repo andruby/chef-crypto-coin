4b0b5175c041e9d6b8ae626ab7abc35fa4fd38b7
Andrew Fecheyr <andrew@bedesign.be>
2013-06-06 10:43:40 +0200

    Fix for bitcoin recompiling

--- recipes/bitcoin.rb
+++ recipes/bitcoin.rb
@@ -4,6 +4,7 @@ include_recipe "crypto-coin::default"
 
 crypto_coin "bitcoin" do
   repository "https://github.com/bitcoin/bitcoin.git"
-  revision   "v0.8.2"
+  # Needed to add ^{} to tag name to stop git from fetching the wrong sha
+  revision  "v0.8.2^{}"
   port       8333
 end
