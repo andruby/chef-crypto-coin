e603fa83e3c177eefd70d8bd56d8cd50746dd3b5
vindimy <vindimy@gmail.com>
2013-09-27 23:33:35 -0400

    Group should also have access to .conf file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrew@bedesign.be"
 license          "MIT"
 description      "Installs/Configures crypto-coin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 
 depends          "apt"
 
